968059245bfd6d8e44af492416f44a91b55dc422
Irving Henriquez <henriquezirv@gmail.com>
2021-05-27 17:02:34 +0000

    completed getAll reservation query function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -20,7 +20,6 @@ const getUserWithEmail = function (email) {
   return pool
     .query(`SELECT * FROM users WHERE email = $1;`, [email])
     .then((users) => {
-      console.log(users.rows[0]);
       return users.rows[0];
     })
     .catch((err) => {
@@ -38,7 +37,6 @@ const getUserWithId = function (id) {
   return pool
     .query(`SELECT * FROM users WHERE id = $1;`, [id])
     .then((users) => {
-      console.log(users.rows[0]);
       return users.rows[0];
     })
     .catch((err) => {
@@ -60,7 +58,6 @@ const addUser = function (user) {
   return pool
     .query(query, values)
     .then((users) => {
-      console.log(users.rows[0]);
       return users.rows[0];
     })
     .catch((err) => {
@@ -77,7 +74,17 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      "SELECT * FROM reservations JOIN properties ON reservations.property_id = properties.id WHERE reservations.guest_id = $1 LIMIT 10;",
+      [guest_id]
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
